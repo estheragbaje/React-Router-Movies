f0e6049be967ce9a98cef1dc3ab9614b70167296
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-16 20:41:26 +0100

    Wrapped App with Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
